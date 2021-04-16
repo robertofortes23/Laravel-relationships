542b2a7875aeb247fbafdac39af01133e4012b0a
Roberto Fortes <robertbernd23@gmail.com>
2021-04-16 14:34:07 -0300

    Criação das migrations.

---
+++ laravel-many-to-many/database/migrations/2018_05_23_135325_alocacao.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Alocacao extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alocacoes', function (Blueprint $table) {
+            $table->integer('desenvolvedor_id')->unsigned();
+            $table->foreign('desenvolvedor_id')->references('id')->on('desenvolvedores');
+            $table->integer('projeto_id')->unsigned();
+            $table->foreign('projeto_id')->references('id')->on('projetos');
+            $table->integer('horas_semanais');
+            $table->primary(['projeto_id', 'desenvolvedor_id']);
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alocacoes');
+    }
+}
